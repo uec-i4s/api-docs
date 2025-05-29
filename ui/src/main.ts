b1741ac975b84a52bb0e91b73f167f8dec5b04ec
ryota2357 <noreply@ryota2357.com>
2025-05-29 17:47:24 +0900

    feat: migrate omron-v1

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -6,6 +6,7 @@ SwaggerUI({
   urls: [
     { url: "./specs/aia-v1.yaml", name: "AIA v1" },
     { url: "./specs/patlite-v1.yaml", name: "Patlite v1" },
+    { url: "./specs/omron-v1.yaml", name: "OMRON v1" },
   ],
   dom_id: "#app",
   deepLinking: true,
