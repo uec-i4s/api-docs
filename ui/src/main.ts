a1357750e013dd6d930aa5e64e83ca686750fb64
ryota2357 <noreply@ryota2357.com>
2025-05-29 16:19:20 +0900

    feat: migrate patlite-v1

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -5,6 +5,7 @@ import "swagger-ui/dist/swagger-ui.css";
 SwaggerUI({
   urls: [
     { url: "./specs/aia-v1.yaml", name: "AIA v1" },
+    { url: "./specs/patlite-v1.yaml", name: "Patlite v1" },
   ],
   dom_id: "#app",
   deepLinking: true,
