99fe7fccdb6f0554e8d2aeea3f8c6d6f2625fd5a
ryota2357 <noreply@ryota2357.com>
2025-06-30 17:34:52 +0900

    fix: missing W9 and E36 api for SwaggerUI

--- swagger-ui/src/main.ts
+++ swagger-ui/src/main.ts
@@ -5,8 +5,10 @@ import "swagger-ui/dist/swagger-ui.css";
 SwaggerUI({
   urls: [
     { url: "./specs/aia-v1.yaml", name: "AIA v1" },
-    { url: "./specs/patlite-v1.yaml", name: "Patlite v1" },
+    { url: "./specs/e36-v1.yaml", name: "E36 v1" },
     { url: "./specs/omron-v1.yaml", name: "OMRON v1" },
+    { url: "./specs/patlite-v1.yaml", name: "Patlite v1" },
+    { url: "./specs/w9-v1.yaml", name: "W9 v1" },
   ],
   dom_id: "#app",
   deepLinking: true,
