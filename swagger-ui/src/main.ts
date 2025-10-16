445d10b264ae29dfca73b892b78831e99a98b77f
ryota2357 <noreply@ryota2357.com>
2025-10-16 16:06:26 +0900

    feat: add OMRON v2 spec

--- swagger-ui/src/main.ts
+++ swagger-ui/src/main.ts
@@ -8,6 +8,7 @@ SwaggerUI({
     { url: "./specs/aia-v2.yaml", name: "AIA v2" },
     { url: "./specs/e36-v1.yaml", name: "E36 v1" },
     { url: "./specs/omron-v1.yaml", name: "OMRON v1" },
+    { url: "./specs/omron-v2.yaml", name: "OMRON v2" },
     { url: "./specs/patlite-v1.yaml", name: "Patlite v1" },
     { url: "./specs/w9-v1.yaml", name: "W9 v1" },
     { url: "http://openapi.i4s.uec.ac.jp/docs/actuator/w9_elevator/openapi.json", name: "W9 Elevator v1" },
